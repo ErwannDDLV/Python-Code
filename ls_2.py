7450f933ecba3ec68b8a08a8f4e074162f650be1
Erwann de Dieuleveult <ededieuleveult@gmail.com>
2018-03-28 22:03:15 +0200

    fix file_list to list all files with specific prefix and add a total count of files in files_lis_mode

--- ls_2.py
+++ ls_2.py
@@ -11,13 +11,20 @@ import time
 from datetime import datetime 
 
 def file_list(path):
+    if os.path.isdir(path):
+        for file in os.listdir(path):
+            if os.path.isfile(os.path.join(path, file)) and not file.startswith('.'):
+                print(file)
+    else:
+        for file in glob.glob(path + '*.*', recursive=True):
+            print(os.path.basename(file))
+            
+def files_list_mode(path):
+    total = []
     for file in os.listdir(path):
         if os.path.isfile(os.path.join(path, file)) and not file.startswith('.'):
-            print(file)
-
-def files_list_mode(path):
-    files_count = len([file for file in os.listdir(path) if os.path.isfile(os.path.join(path, file)) and file[0] != '.'])
-    print('total : %s' % files_count)
+            total.append(file)
+    print('total : %s' % len(total))
 
     for file in os.listdir(path):
         pathname = os.path.join(path, file)
@@ -31,12 +38,6 @@ def files_list_mode(path):
                 file)
             )
 
-# def prefix_files(path, basename):
-#     for file in glob.glob(os.path.join(path, '?.*')):
-#         if os.path.isfile(os.path.join(path, file)):
-#             print(os.path.basename(file))
-
-
 def main():
 
     parser = argparse.ArgumentParser(
@@ -47,23 +48,11 @@ def main():
     )
         
     parser.add_argument("path", help='path to your folder')
-    # parser.add_argument("basename", help='basename file')
-
     parser.add_argument('-l', "--files_list_mode", action = 'store_const', const = files_list_mode, help='List files from your directory with mode and creation date')
-    parser.add_argument('-p', "--prefix_files", type=str, help='enter the 4 first letters to list the files from your directory')
-    # action = 'store_const', const = prefix_files,
 
     args = parser.parse_args()
 
     if args.files_list_mode: files_list_mode(args.path)
-    
-    # elif args.prefix_files: prefix_files(args.path, args.prefix_files)
-    elif args.prefix_files:
-        basename = args.prefix_files
-        for file in glob.glob(os.path.join(args.path, basename + '*.*')):
-            if os.path.isfile(os.path.join(args.path, file)):
-                print(os.path.basename(file))
-
     elif args.path: file_list(args.path)
 
 if __name__ == '__main__':
