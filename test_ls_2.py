9a9b21b9a6380c17e958d41e7caaead5ecb9e400
Erwann de Dieuleveult <ededieuleveult@gmail.com>
2018-03-29 13:47:30 +0200

    add tests

--- test_ls_2.py
+++ test_ls_2.py
@@ -1,16 +1,25 @@
 import unittest
 import ls_2
 import os
+import stat
+import time
+
+from datetime import datetime 
 
 # run test file with :
-# python3 -m unittest test_ls_2.py
+# python3 test_ls_2.py
 
-
-class TestLs2(unittest.TestCase):
+class TestFileList(unittest.TestCase):
 
     def test_file_list(self):
-        result = ls_2.file_list('/Users/erwann/code/ErwannDDLV/Python-code/testing-folder/')
-        self.assertEqual([
+        path = "/Users/erwann/code/ErwannDDLV/Python-Code/testing-folder/"
+        files_list = []
+
+        if os.path.isdir(path):
+            for file in os.listdir(path):
+                if os.path.isfile(os.path.join(path, file)) and not file.startswith('.'):
+                    files_list.append(file)
+            self.assertEqual(files_list, [
                 'aa-test.doc',
                 'aa-two-test.txt',
                 'bb-test.txt',
@@ -19,7 +28,52 @@ class TestLs2(unittest.TestCase):
                 'test.doc',
                 'test.txt',
                 'test.xls'
-        ], result)
+            ])
+            self.assertNotEqual(files_list, [
+                'aa-test.doc',
+                'test-two.doc',
+                'test.doc',
+                'test.txt',
+                'test.xls'
+            ])       
+
+        else:
+            for file in glob.glob(path + 't*.*', recursive=True):
+                    files_list.append(file)
+            self.assertEqual(files_list, [
+                'test-two.doc',
+                'test.doc',
+                'test.txt',
+                'test.xls'
+            ])
+            self.assertNotEqual(files_list, [
+                'aa-test.doc',
+                'aa-two-test.txt',
+                'bb-test.txt',
+                'new-test-file.xls',
+            ])       
+
+    def test_file_list_mode(self):
+        path = "/Users/erwann/code/ErwannDDLV/Python-Code/testing-folder/"
+        files_list = []
+
+        for file in os.listdir(path):
+            pathname = os.path.join(path, file)
+            permissions = stat.filemode(os.stat(pathname).st_mode)
+            created_date = datetime.fromtimestamp(os.stat(pathname).st_ctime).date()
+            if os.path.isfile(pathname) and not file.startswith('.'):
+                files_list.append(file)
+        self.assertEqual(files_list, [
+            'aa-test.doc',
+            'aa-two-test.txt',
+            'bb-test.txt',
+            'new-test-file.xls',
+            'test-two.doc',
+            'test.doc',
+            'test.txt',
+            'test.xls'
+        ])
 
 if __name__ == '__main__':
     unittest.main()
+
